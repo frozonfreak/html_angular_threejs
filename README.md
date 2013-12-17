8235edbbdc3890aaff958b7250f0e777226fddd9
FrozonFreak <shazcool@gmail.com>
2013-12-17 17:25:49 +0800

    merge scene.js and app.js
    moved scene code into controller

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AngularJS HTML5 ThreeJS 
 
-	Testing of ThreeJS with AngularJS
-	Reference Code [Plnkr](http://plnkr.co/edit/rBp1NMGTm6ypv58suD6n?p=preview)
+Testing of ThreeJS with AngularJS
+Reference Code [Plnkr](http://plnkr.co/edit/rBp1NMGTm6ypv58suD6n?p=preview)
+
+Moved `scene.js` code into angular controller
 
--- js/app.js
+++ js/app.js
@@ -22,19 +22,115 @@ angular3D.config(function($stateProvider, $urlRouterProvider) {
 //controller
 angular3D.controller('appController', function($scope, $timeout){
     
-  
-
   //Rotation variables
   var xrotation;
   var yrotation;
   var zrotation;
+  var WIDTH = 400;
+  var HEIGHT = 300;
 
+  //Set Aspect
+  var ASPECT = WIDTH / HEIGHT;
+
+  // create a WebGL renderer
+  // and a scene
+  var renderer = new THREE.WebGLRenderer({antialias: true});
+  var scene = new THREE.Scene();
+
+  //Create Cube
+  var cube;
+
+  //Get Cube rotation details
+  $scope.getCubeRotation = function() {
+      return [cube.rotation.x, cube.rotation.y, cube.rotation.z];
+  };
 
   //Initializer
+  $scope.createGeometry = function() {
+      // set up the cube vars
+      var length = 50;
+      var segments = 16;
+
+      // create the cube's material
+      var sphereMaterial = new THREE.MeshLambertMaterial({
+          color: 0xCC0000
+      });
+
+      // create a new mesh with cube geometry -
+      cube = new THREE.Mesh(
+             new THREE.CubeGeometry(length, length, length, segments, segments, segments),
+                  sphereMaterial);
+
+      //Set Cube Rotation
+      cube.rotation.x += 0.2;
+      cube.rotation.y += 0.3;
+      cube.rotation.z += 0.1;
+
+      // add the cube to the scene
+      scene.add(cube);
+  };
+
+  $scope.createCamera = function() {
+      // set some camera attributes
+      var VIEW_ANGLE = 45;
+      var NEAR = 0.1;
+      var FAR = 10000;
+
+      // create a WebGL camera
+      camera = new THREE.PerspectiveCamera(VIEW_ANGLE,
+                    ASPECT,
+                    NEAR,
+                    FAR);
+
+      // the camera starts at 0,0,0 so pull it back
+      camera.position.z = 250;
+
+      // and the camera
+      scene.add(camera);
+  };
+
+  $scope.createLight = function() {
+      // create a point light
+      var pointLight = new THREE.PointLight(0xFFFFFF);
+
+      // set its position
+      pointLight.position.x = 10;
+      pointLight.position.y = 50;
+      pointLight.position.z = 130;
+
+      // add to the scene
+      scene.add(pointLight);
+  };
+
+  $scope.paint = function() {
+      // draw!
+      renderer.render(scene, camera);
+  };
+
+  $scope.setup = function(){
+      // start the renderer
+      renderer.setSize(WIDTH, HEIGHT);
+
+      // get the DOM element to attach to
+      // - assume we've got jQuery to hand
+      var $container = $('#container1');
+
+      // attach the render-supplied DOM element
+      $container.append(renderer.domElement);
+  };
+
 	init();
 	function init(){
-		scene.init();
-		var position = scene.getCubeRotation();
+        
+		//scene.init();
+    $scope.createCamera();
+    $scope.createLight();
+    $scope.createGeometry();
+    $scope.setup();
+    $scope.paint();
+
+
+		var position = $scope.getCubeRotation();
 
     $scope.x = position[0];
     $scope.y = position[1];
@@ -49,26 +145,29 @@ angular3D.controller('appController', function($scope, $timeout){
     $scope.labelZ = 'Z';
     $scope.axisZ = $scope.z;
 	};
-  
+      
   //Color Update
   $scope.updateRedColor = function(){
-      scene.setCubeRedColorAndPaint();
+      cube.material.color.setHex(0xCC0000);
+      $scope.paint();
   };
   $scope.updateGreenColor = function(){
-      scene.setCubeGreenColorAndPaint();
+      cube.material.color.setHex(0x00CC00);
+      $scope.paint();
   };
   $scope.updateBlueColor = function(){
-      scene.setCubeBlueColorAndPaint();
+      cube.material.color.setHex(0x0000CC);
+      $scope.paint();
   };
+
 
   //Rotation Update X
   $scope.updateRotationX = function () {
-
       this.x = this.axisX;
-      scene.setCubeXRotationAndPaint(this.x);
+      cube.rotation.x = this.x;
+      $scope.paint();
   }
   
-
   $scope.rotateX  = function(delta, timeout) {
     xrotation = $timeout(function() {
       $scope.axisX = $scope.axisX + delta;
@@ -83,13 +182,14 @@ angular3D.controller('appController', function($scope, $timeout){
     console.log('Stoped rotation of X Axis');
   };  
 
+
   //Rotation Update Y
   $scope.updateRotationY = function () {
-
       this.y = this.axisY;
-      scene.setCubeYRotationAndPaint(this.y);
+      cube.rotation.y = this.y;
+      $scope.paint();
+
   };
-  
   
   $scope.rotateY  = function(delta, timeout) {
     yrotation = $timeout(function() {
@@ -104,14 +204,13 @@ angular3D.controller('appController', function($scope, $timeout){
     console.log('Stoped rotation of Y Axis');
   };
 
-
+ 
   //Rotation Update Z Axis
   $scope.updateRotationZ = function () {
-
       this.z = this.axisZ;
-      scene.setCubeZRotationAndPaint(this.z);
+      cube.rotation.z = this.z;
+      $scope.paint();
   };
-  
   
   $scope.rotateZ  = function(delta, timeout) {
     zrotation = $timeout(function() {
